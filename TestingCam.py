310a26b01247350e14a0e1255d9497144c522362
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2023-12-30 01:03:48 -0600

    Create TestingCam.py

---
+++ TestingCam.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from gpiozero import Button
+... from picamera import PiCamera
+... from datetime import datetime
+... from signal import pause
+... 
+... button = Button(2)
+... camera = PiCamera()
+... 
+... def capture():
+...     timestamp = datetime.now().isoformat()
+...     camera.capture('/home/pi/%s.jpg' % timestamp)
+... 
+... button.when_pressed = capture
+... 
