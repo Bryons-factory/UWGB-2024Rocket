9eb92bd560972916ea926bea1e82d61a7d7b165f
tabula_razma <148983026+Gallitzur@users.noreply.github.com>
2024-01-09 16:33:10 -0600

    Create esp32webcamservercombo.ino

---
+++ esp32WCSC/esp32webcamservercombo.ino
@@ -0,0 +1,394 @@
+/*********
+UWGB Web Cam Server Combo_MicroSD enable - RocketClub Version 1.0 ESP32WEBCAMSERVERCOMBO
+
+
+*********/
+//cam libraries
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "driver/rtc_io.h"
+
+//microSD Libraries
+#include "FS.h"
+#include "SD_MMC.h"
+
+////EEPROM Library //number as file for images we save
+#include "EEPROM.h"
+
+// Load Wi-Fi library
+#include <WiFi.h>
+
+//use 1 byte of EEPROM space
+#define EEPROM_SIZE 1
+
+//counter for picture number
+unsigned int pictureCount = 0;
+
+
+//Pin definitions for CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+void configESPCamera() {
+  //config cam param
+  //obj to store the cam config param
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; // choices are YUV422, GRAYSCALE, RGB565, JPEG
+
+  //select lower framesize if camera dont support PSRAM
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA; // framesize_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10; //10-63 loer number means higher quality
+    config.fb_count = 2;
+
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+  }
+
+  //init the camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  //camera quality adjustments
+  sensor_t * s = esp_camera_sensor_get();
+  //BRIGHTNESS (-2 to 2)
+  s->set_brightness(s,0);
+    //contrast (-2 to 2)
+  s->set_contrast(s,0);
+    //saturation (-2 to 2)
+  s->set_saturation(s,0);
+    //special effects (0 - no effect, 1 - negativce, 2 grayscale, 3 red tint, 4green tint, 5 blue tint)
+  s->set_special_effect(s,0);
+    //white_bal (0 disable 1 enable)
+  s->set_whitebal(s,1);
+    //AWB GAIN (0 = Disable, 1 = Enable)
+  s->set_awb_gain(s,1);
+    //WB MODES (0 auto, 1 sunny, 2 cloudy, 3 office, 4 home)
+  s->set_wb_mode(s,0);
+
+      //Exposure control
+  s->set_exposure_ctrl(s,1);
+    //AEC2 (0 disable 1 enable)
+  s->set_aec2(s,0);
+    //AE LEVELS (-2 to 2)
+  s->set_ae_level(s,0);
+    //AEC VALUES (0 to 1200)
+  s->set_aec_value(s,300);
+  //GAIN CONTROLS (0 = disable, 1 = enable)
+  s->set_gain_ctrl(s,1);
+  //AGC GAIN (0 to 30)
+  s->set_agc_gain(s,0);
+  //GAIN CEILING (0 to 6)
+  s->set_gainceiling(s, (gainceiling_t)0);
+  //BPC(0 disable, 1 enable)
+  s->set_bpc(s,0);
+  //WPC (0 disable 1 enable)
+  s->set_wpc(s,1);
+   // RAW GMA (0 = Disable , 1 = Enable)
+  s->set_raw_gma(s, 1);
+  // LENC (0 = Disable , 1 = Enable)
+  s->set_lenc(s, 1);
+  // HORIZ MIRROR (0 = Disable , 1 = Enable)
+  s->set_hmirror(s, 0);
+  // VERT FLIP (0 = Disable , 1 = Enable)
+  s->set_vflip(s, 0);
+  // DCW (0 = Disable , 1 = Enable)
+  s->set_dcw(s, 1);
+  // COLOR BAR PATTERN (0 = Disable , 1 = Enable)
+  s->set_colorbar(s, 0);
+}
+
+void initMicroSDCard() {
+  // Start the MicroSD card
+ 
+  Serial.println("Mounting MicroSD Card");
+  if (!SD_MMC.begin()) {
+    Serial.println("MicroSD Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("No MicroSD Card found");
+    return;
+  }
+ 
+}
+
+void takeNewPhoto(String path) {
+  // Take Picture with Camera
+ 
+  // Setup frame buffer
+  camera_fb_t  * fb = esp_camera_fb_get();
+ 
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+ 
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file in write mode");
+  }
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+  }
+  // Close the file
+  file.close();
+ 
+  // Return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
+}
+
+
+// Replace with your network credentials connect to your hotspot from ESP32 and then access webserver via IP on browser
+const char* ssid = "rocketclub";
+const char* password = "rocketadmin";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
+String ledState = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
+// ledPin refers to ESP32-CAM GPIO 4 (flashlight)
+const int ledPin = 4;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
+  digitalWrite(ledPin, LOW);
+
+
+  
+  //init camera
+  Serial.print("Initializing the camera module...");
+  configESPCamera();
+  Serial.println("Camera OK!");
+  
+  //init microSD
+  Serial.print("Initializing the MicroSD card module... ");
+  initMicroSDCard();
+
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+ 
+
+
+
+
+
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+
+
+
+
+
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off and camera
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+              Serial.println("GPIO 27 on");
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              Serial.println("GPIO 27 off");
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }
+            else if (header.indexOf("GET /led/on") >= 0) {
+              Serial.println("camLED on");
+              ledState = "on";
+              digitalWrite(ledPin, HIGH);
+            } 
+            else if (header.indexOf("GET /led/off") >= 0) {
+              Serial.println("camLED off");
+              ledState = "off";
+              digitalWrite(ledPin, LOW);
+            }
+            else if (header.indexOf("GET /image/capture") >= 0) {
+              Serial.println("Image Capture");
+              pictureCount = EEPROM.read(0) + 1;
+              // Path where new picture will be saved in SD Card
+              String path = "/image" + String(pictureCount) + ".jpg";
+              //Take and Save Photo
+              takeNewPhoto(path);
+              //Print to serial
+              Serial.printf("Picture file name: %s\n", path.c_str());
+              // Update EEPROM picture number counter
+              EEPROM.write(0, pictureCount);
+              EEPROM.commit();
+              }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>UWGB - Rocketcam</h1>");
+            // Display current state, and ON/OFF buttons for Cam LED
+            client.println("<p>Cam LED - State " + ledState + "</p>");
+             if (ledState=="off") {
+              client.println("<p><a href=\"/led/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/led/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            //Display Capture Image button
+            client.println("<p>Camera Status... </p>");
+            client.println("<p><a href=\"/image/capture\"><button class=\"button button3\">Capture Image</button></a></p>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
